9a72b1e0d5a36f29644d1b91f389ac5d4cacd3bd
simonojok19 <simonojok19@gmail.com>
2020-07-02 19:55:34 +0300

    implemented the activity

--- app/src/main/java/org/ygba/youthgobudget/AgricListActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/AgricListActivity.java
@@ -4,11 +4,241 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.amigold.fundapter.BindDictionary;
+import com.amigold.fundapter.FunDapter;
+import com.amigold.fundapter.extractors.StringExtractor;
+import com.kosalgeek.android.json.JsonConverter;
+import com.kosalgeek.genasync12.AsyncResponse;
+import com.kosalgeek.genasync12.EachExceptionsHandler;
+import com.kosalgeek.genasync12.PostResponseAsyncTask;
+
+import org.ygba.youthgobudget.database.DatabaseHelper;
+import org.ygba.youthgobudget.utils.Goapphelper;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.ygba.youthgobudget.utils.Goapphelper.IP_ADDRESS;
+
 public class AgricListActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
+    ArrayList<Goapphelper> nacHelpers;
+    private String s;
+    Goapphelper data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agric_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("List of records");
+
+        ListView lv;
+
+        FloatingActionButton fab =  findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Uploading your data", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int choice) {
+                        switch (choice) {
+                            case DialogInterface.BUTTON_POSITIVE:
+
+                                databaseHelper=new DatabaseHelper(AgricListActivity.this);
+                                s=String.valueOf(databaseHelper.cur2Json(databaseHelper.reaData()));
+
+                                Log.d("SQL-JSON",s);
+
+                                Toast.makeText(AgricListActivity.this, "Starting to upload data ...", Toast.LENGTH_SHORT).show();
+
+                                HashMap hashMap = new HashMap();
+                                hashMap.put("DATA",s);
+                                PostResponseAsyncTask postResponseAsyncTask = new PostResponseAsyncTask(AgricListActivity.this, hashMap, new AsyncResponse() {
+                                    @Override
+                                    public void processFinish(String s) {
+                                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AgricListActivity.this);
+                                        alertDialogBuilder.setTitle("Server Response");
+                                        alertDialogBuilder.setMessage(s);
+                                        AlertDialog alertDialog = alertDialogBuilder.create();
+                                        alertDialog.show();
+                                        return;
+                                    }
+                                });
+
+                                postResponseAsyncTask.execute(IP_ADDRESS+"collect/agric");
+                                postResponseAsyncTask.setEachExceptionsHandler(new EachExceptionsHandler() {
+                                    @Override
+                                    public void handleIOException(IOException e) {
+                                        Toast.makeText(getApplicationContext(), "Request Could not reach the dedicated server, operation failed.", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void handleMalformedURLException(MalformedURLException e) {
+                                        Toast.makeText(getApplicationContext(), "The URL is not well specified.", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void handleProtocolException(ProtocolException e) {
+                                        Toast.makeText(getApplicationContext(), "Issue with protocol.", Toast.LENGTH_LONG).show();
+                                    }
+
+                                    @Override
+                                    public void handleUnsupportedEncodingException(UnsupportedEncodingException e) {
+                                        Toast.makeText(getApplicationContext(), "Text encoding is not proper.", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(AgricListActivity.this);
+                builder.setTitle("Upload Record(s)");
+                builder.setMessage("All your new records will be uploaded to the remote server, your data will stay on the phone for backup purposes.").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+
+            }
+        });
+
+
+        lv=(ListView) findViewById(R.id.records);
+
+        databaseHelper=new DatabaseHelper(AgricListActivity.this);
+
+        s=String.valueOf(databaseHelper.cur2Json(databaseHelper.reaData()));
+
+        nacHelpers = new JsonConverter<Goapphelper>().toArrayList(s, Goapphelper.class);
+
+        BindDictionary<Goapphelper> dict = new BindDictionary<Goapphelper>();
+
+        dict.addStringField(R.id.key_id, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Record ID: "+item.id;
+            }
+        });
+
+        dict.addStringField(R.id.region, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Region: "+item.region;
+            }
+        });
+
+        dict.addStringField(R.id.district, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "District: "+item.district;
+            }
+        });
+
+        dict.addStringField(R.id.subcounty, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Sub-County: "+item.subcounty;
+            }
+        });
+
+        dict.addStringField(R.id.parish, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Parish: "+item.parish;
+            }
+        });
+
+        dict.addStringField(R.id.phone_id, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Phone id: "+item.phone_id;
+            }
+        });
+
+        dict.addStringField(R.id.staffName, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Staff Name: "+item.staffName;
+            }
+        });
+
+        dict.addStringField(R.id.date_recorded, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Date recorded: "+item.date_recorded;
+            }
+        });
+
+        dict.addStringField(R.id.ageCategory, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Age Category: "+item.ageCategory;
+            }
+        });
+
+        dict.addStringField(R.id.gender, new StringExtractor<Goapphelper>() {
+            @Override
+            public String getStringValue(Goapphelper item, int position) {
+                return "Gender: "+item.gender;
+            }
+        });
+
+
+
+
+        FunDapter<Goapphelper> fundupter = new FunDapter<>(AgricListActivity.this, nacHelpers, R.layout.records_layout, dict);
+        lv.setAdapter(fundupter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                data = nacHelpers.get(position);
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int choice) {
+                        switch (choice) {
+                            case DialogInterface.BUTTON_POSITIVE:
+
+                                DatabaseHelper databaseHelper = new DatabaseHelper(AgricListActivity.this);
+                                databaseHelper.deleteSingleRow(data.id);
+
+                                finish();
+                                startActivity(getIntent());
+
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(AgricListActivity.this);
+                builder.setTitle("Delete Record");
+                builder.setMessage("Are you sure you want to delete this record?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+            }
+        });
     }
 }
