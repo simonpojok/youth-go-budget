e0eaf60814227ac66264f2da241feb3d713150c7
simonojok19 <simonojok19@gmail.com>
2020-07-02 18:50:42 +0300

    added splash complete

--- app/src/main/java/org/ygba/youthgobudget/SplashActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/SplashActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SpashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
