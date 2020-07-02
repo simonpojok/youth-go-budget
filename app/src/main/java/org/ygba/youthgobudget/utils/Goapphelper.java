01a3734c2ac20f9018fe685b7de87fc291d18336
simonojok19 <simonojok19@gmail.com>
2020-07-02 19:44:38 +0300

    implemented

--- app/src/main/java/org/ygba/youthgobudget/utils/Goapphelper.java
+++ app/src/main/java/org/ygba/youthgobudget/utils/Goapphelper.java
@@ -1,4 +1,214 @@
 package org.ygba.youthgobudget.utils;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Charles on 7/30/2018.
+ */
+
 public class Goapphelper {
+
+    public static final String IP_ADDRESS = "http://167.179.92.196/api/";
+    @SerializedName("record_id")
+    public String id;
+
+    @SerializedName("title")
+    public String title;
+
+    @SerializedName("sector_id")
+    public String sector_id;
+
+
+    @SerializedName("link")
+    public String link;
+
+    @SerializedName("name_of_monitor")
+    public String name_of_monitor;
+
+    @SerializedName("created_at")
+    public String created_at;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("region_name")
+    public String region_name;
+
+    @SerializedName("quater")
+    public String quater;
+
+    @SerializedName("amount_sent")
+    public String amount_sent;
+
+    @SerializedName("amount")
+    public String amount;
+
+    @SerializedName("sub_county")
+    public String sub_county;
+
+    @SerializedName("date")
+    public String date;
+
+    @SerializedName("sector_name")
+    public String sector_name;
+
+    @SerializedName("date_of_monitoring")
+    public String date_of_monitoring;
+
+    @SerializedName("region")
+    public String region;
+
+    @SerializedName("region_id")
+    public String region_id;
+
+    @SerializedName("district_id")
+    public String district_id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("district")
+    public String district;
+
+    @SerializedName("subcounty")
+    public String subcounty;
+
+    @SerializedName("parish")
+    public String parish;
+
+    @SerializedName("phone_id")
+    public String phone_id;
+
+    @SerializedName("staffName")
+    public String staffName;
+
+    @SerializedName("date_recorded")
+    public String date_recorded;
+
+    @SerializedName("ageCategory")
+    public String ageCategory;
+
+    @SerializedName("Agric_service_best")
+    public String Agric_service_best;
+
+    @SerializedName("gender")
+    public String gender;
+
+    @SerializedName("agric_service_spec")
+    public String agric_service_spec;
+
+    @SerializedName("agric_service_reason")
+    public String agric_service_reason;
+
+    @SerializedName("agric_service_worst")
+    public String agric_service_worst;
+
+    @SerializedName("agric_service_worst_spec")
+    public  String agric_service_worst_spec;
+
+    @SerializedName("agric_service_reason_worst")
+    public String agric_service_reason_worst;
+
+    @SerializedName("Agric_service_problem")
+    public String Agric_service_problem;
+
+    @SerializedName("Agric_service_problem_spec")
+    public String Agric_service_problem_spec;
+
+    @SerializedName("Agric_service_reason_problem")
+    public String Agric_service_reason_problem;
+
+    @SerializedName("Agric_service_need")
+    public String Agric_service_need;
+
+    @SerializedName("Agric_service_need_spec")
+    public String Agric_service_need_spec;
+
+    @SerializedName("Agric_service_reason_need")
+    public String Agric_service_reason_need;
+
+    @SerializedName("Agric_proposed_priority")
+    public String Agric_proposed_priority;
+
+    @SerializedName("Agric_proposed_priority_spec")
+    public String Agric_proposed_priority_spec;
+
+    @SerializedName("Agric_proposed_priority_reason")
+    public String Agric_proposed_priority_reason;
+
+    @SerializedName("level_of_education")
+    public String level_of_education;
+
+    @SerializedName("establish_health_education")
+    public String establish_health_education;
+
+    @SerializedName("yes_establish_health_education")
+    public String yes_establish_health_education;
+
+    @SerializedName("reason_establish_health_education")
+    public String reason_establish_health_education;
+
+    @SerializedName("rate_establish_health_education")
+    public String rate_establish_health_education;
+
+    @SerializedName("rate_reason_establish_health_education")
+    public String rate_reason_establish_health_education;
+
+    @SerializedName("best_health_education")
+    public String best_health_education;
+
+    @SerializedName("best_health_education_spec")
+    public String best_health_education_spec;
+
+    @SerializedName("best_health_education_reason")
+    public String best_health_education_reason;
+
+    @SerializedName("worst_health_education")
+    public String worst_health_education;
+
+    @SerializedName("worst_health_education_spec")
+    public String worst_health_education_spec;
+
+    @SerializedName("worst_health_education_reason")
+    public String worst_health_education_reason;
+
+    @SerializedName("priority_health_education")
+    public String priority_health_education;
+
+    @SerializedName("priority_health_education_spec")
+    public String priority_health_education_spec;
+
+    @SerializedName("priority_health_education_reason")
+    public String priority_health_education_reason;
+
+    @SerializedName("use_education")
+    public String use_education;
+
+    @SerializedName("best_use_education_service")
+    public String best_use_education_service;
+
+    @SerializedName("best_use_education_spec")
+    public String best_use_education_spec;
+
+    @SerializedName("best_use_education_reason")
+    public String best_use_education_reason;
+
+    @SerializedName("worst_use_education_service")
+    public String worst_use_education_service;
+
+    @SerializedName("worst_use_education_spec")
+    public String worst_use_education_spec;
+
+    @SerializedName("worst_use_education_reason")
+    public String worst_use_education_reason;
+
+    @SerializedName("priority_use_education_service")
+    public String priority_use_education_service;
+
+    @SerializedName("priority_use_education_spec")
+    public String priority_use_education_spec;
+
+    @SerializedName("priority_use_education_reason")
+    public String priority_use_education_reason;
 }
+
