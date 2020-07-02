62942aeffe1293c8180f8861b54754c6199b25dc
simonojok19 <simonojok19@gmail.com>
2020-07-02 19:10:41 +0300

    added WaterSectorRecordsActivity

---
+++ app/src/main/java/org/ygba/youthgobudget/WaterSectorRecordsActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaterSectorRecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_sector_records);
+    }
+}
