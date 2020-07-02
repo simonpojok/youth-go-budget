e8cceed72709b554ecffbda2cd39c568e54f4f7d
simonojok19 <simonojok19@gmail.com>
2020-07-02 20:19:16 +0300

    added some codes

--- app/src/main/java/org/ygba/youthgobudget/PollAnswerActivity.java
+++ app/src/main/java/org/ygba/youthgobudget/PollAnswerActivity.java
@@ -4,11 +4,118 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class PollAnswerActivity extends AppCompatActivity {
+
+    private Intent intent;
+
+    private String poll_id,poll_title,poll_link;
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_poll_answer2);
+        setContentView(R.layout.activity_poll_answer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        intent = getIntent();
+
+        poll_id = intent.getStringExtra("poll_id");
+
+        poll_title = intent.getStringExtra("poll_title");
+
+        poll_link = intent.getStringExtra("poll_link");
+
+        getSupportActionBar().setTitle(poll_title);
+
+
+        webView = findViewById(R.id.webView1);
+
+        startWebView(poll_link);
+
+
     }
+
+    private void startWebView(String url) {
+
+        //Create new webview Client to show progress dialog
+        //When opening a url or click on link
+
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            //If you will not use this method url links are opeen in new brower not in webview
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            //Show loader on url load
+            public void onLoadResource (WebView view, String url) {
+                if (progressDialog == null) {
+                    // in standard case YourActivity.this
+                    progressDialog = new ProgressDialog(PollAnswerActivity.this);
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+            public void onPageFinished(WebView view, String url) {
+                try{
+                    if (progressDialog.isShowing()) {
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    }
+                }catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+
+        webView.loadUrl(url);
+
+
+    }
+
+    // Open previous opened link from history on webview when back button pressed
+
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
+    }
+
 }
