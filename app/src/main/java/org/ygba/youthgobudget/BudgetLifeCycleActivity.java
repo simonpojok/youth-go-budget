c4ac6d212161fce91c7145ceeb606821fcd9ac01
simonojok19 <simonojok19@gmail.com>
2020-07-02 19:04:13 +0300

    added thee

---
+++ app/src/main/java/org/ygba/youthgobudget/BudgetLifeCycleActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BudgetLifeCycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_life_cycle);
+    }
+}
