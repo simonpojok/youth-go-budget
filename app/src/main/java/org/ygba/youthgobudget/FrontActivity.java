4fabb7a7bdc778f28d33bdb5046c5154a7604362
simonojok19 <simonojok19@gmail.com>
2020-07-02 18:33:44 +0300

    added the FrontActivity

---
+++ app/src/main/java/org/ygba/youthgobudget/FrontActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
