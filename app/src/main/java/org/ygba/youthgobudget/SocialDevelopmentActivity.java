977ebb3c86ed156c49ae4605ac33a375813f954b
simonojok19 <simonojok19@gmail.com>
2020-07-02 19:10:00 +0300

    added the nest activity

---
+++ app/src/main/java/org/ygba/youthgobudget/SocialDevelopmentActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialDevelopmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_development);
+    }
+}
