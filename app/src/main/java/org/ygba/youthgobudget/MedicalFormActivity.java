48283921cdf061b9a23dc5afde311687fab9f1fe
simonojok19 <simonojok19@gmail.com>
2020-07-02 18:41:57 +0300

    added MedicalFormActivity files

---
+++ app/src/main/java/org/ygba/youthgobudget/MedicalFormActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicalFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_form);
+    }
+}
