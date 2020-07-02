3caa401df6362903e0ab0e8f4bb674f121cbb21b
simonojok19 <simonojok19@gmail.com>
2020-07-02 18:53:13 +0300

    added another BudgetMonitoringRecordsActivity

---
+++ app/src/main/java/org/ygba/youthgobudget/BudgetMonitoringRecordsActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BudgetMonitoringRecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget_monitoring_records);
+    }
+}
