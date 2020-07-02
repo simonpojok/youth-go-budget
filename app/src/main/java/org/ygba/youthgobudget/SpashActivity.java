7259476bf8e2e3d546c10d8ce900aaa6dd8f0cd7
simonojok19 <simonojok19@gmail.com>
2020-07-02 18:49:16 +0300

    added splash screen

---
+++ app/src/main/java/org/ygba/youthgobudget/SpashActivity.java
@@ -0,0 +1,14 @@
+package org.ygba.youthgobudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash);
+    }
+}
